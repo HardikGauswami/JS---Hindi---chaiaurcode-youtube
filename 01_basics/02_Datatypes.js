3e47a0ca13aeb531a7cf190870c5270c17953413
HardikGodwami190 <hardikgoswami095@gmail.com>
2024-08-06 05:50:22 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) //we are using node js not browser
+
+console.log(3 
+    + 3) // code readability should be high
+console.log("Hardik")
+
+let name = "Hardik" // string datatype
+let age = 20 // number datatype
+let isLoggedIn = false // boolean 
+
+// primitive datatypes
+//number 234
+//string "xyz"
+//boolean true/false
+//bigint  -- use in largest nubers
+//null => standalone value let a = null
+//undefined ==> let state;
+//symbol => unique
+
+//object
+
+//type of datatypes
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+
+//type of null
+
+console.log(typeof null); // object - important!
+
