87f3411374422333bb48758a5541212083e4545f
HardikGodwami190 <hardikgoswami095@gmail.com>
2024-08-06 12:18:34 +0000

    modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "hardik"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 //Conversion of Number to ---
 
@@ -21,7 +21,7 @@ let isloggedIn = undefined
 
 let booleanIsLoggedIn = Boolean(isloggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -32,6 +32,44 @@ console.log(booleanIsLoggedIn);
 let someNumber = 44
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//********************** Operations ******************/
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(22+2);
+// console.log(22-2);
+// console.log(22*2);
+// console.log(4**2);
+// console.log(22/2);
+// console.log(29%2);
+
+let str1 = "hello"
+let str2 = " hardik"
+
+let str3 = str1 + str2;
+// console.log(str3); // hello hardik
+
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
 
