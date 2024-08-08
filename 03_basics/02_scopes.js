14a216ec156ff8b1fdf97d882c053ebe7a58eeb7
Hardik Gauswami <hardikgoswami095@gmail.com>
2024-08-08 05:27:56 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,55 @@ if (true) {
     let a = 10
     const b = 20
      c = 30
-     console.log(a);
+    //  console.log(a);
      
 } //block scope
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "Hardik"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    } // function two can access all variables and methods of function one
+    // console.log(website);
+    
+    two()
+} // function one can not access inside elements of function two
+
+// one()
+
+if(true){
+    const user = "hardik"
+    if(user === "hardik"){
+      const website =  " youtube"
+    //   console.log(user + website);
+      
+    }
+    // console.log(website);
+    
+}
+//  console.log(user);
+ 
+//+++++++++++++++++++++++++ interesting +++++++++++++++
+
+console.log(addone(5)) // 6
+
+function addone(num){
+     return num + 1
+} // normal function
+
+ 
+console.log(addTwo(5)) //error can not access before intiallizing
+
+const addTwo  = function(num){
+    return num + 2
+} //function (expression) 
+
+
+
